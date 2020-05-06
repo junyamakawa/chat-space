9e556d625926947f20562c3e57f8a8cb4738ceec
junyamakawa <udonyamakawa@gmail.com>
2020-05-06 21:14:08 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function(){ 
   var last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
   function buildHTML(message){
     if ( message.image ) {
         var html =
@@ -74,7 +73,6 @@ $(function(){
         data: {id: last_message_id}
       })
       .done(function(messages) {
-          // console.log(messages);
         if (messages.length !== 0) {
           var insertHTML = '';
           $.each(messages, function(i, message) {
